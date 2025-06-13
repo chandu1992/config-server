0d71c62d342c627e93fae486e556262e48aa8616
chandrashekharbhaupande <chpande55@gmail.com>
2025-06-13 22:57:39 +0530

    data

--- src/main/java/com/jarvis/config_server/ConfigServerApplication.java
+++ src/main/java/com/jarvis/config_server/ConfigServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
-
+//	System.out.Print("this is config server")
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
